b49806dbd8df6a7215da1b7dfd9ebbfe8c65fef0
Jonatan Shoshan <jonatan-bs@live.dk>
2020-04-09 20:44:07 +0200

    imageLib CRUD

--- server/models/initModels.js
+++ server/models/initModels.js
@@ -1,2 +1,3 @@
 require("./user");
 require("./userRole");
+require("./imageLibrary");
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,7 +1,30 @@
 const express = require("express");
+const multer = require("multer"); // Handle file uploads
 const router = express.Router();
 const userCtrl = require("../controllers/user");
 const userRoleCtrl = require("../controllers/userRole");
+const imageLibraryCtrl = require("../controllers/imageLibrary");
+
+// MULTER MIDDLEWARE
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, "public/uploads/");
+  },
+  filename: function (req, file, callback) {
+    callback(null, new Date().toISOString() + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, callback) => {
+  if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
+    // Accept file
+    callback(null, true);
+  } else {
+    // reject file
+    callback(null, false);
+  }
+};
+const upload = multer({ storage: storage, fileFilter: fileFilter }); // Handle file uploads
 
 /////////////////
 //// USERS
@@ -29,5 +52,22 @@ router.post("/userrole/delete/:id", userRoleCtrl.delete);
 
 // update userroles
 router.post("/userrole/update/:id", userRoleCtrl.update);
+
+/////////////////
+//// IMAGE LIBRARY
+/////////////////
+
+// create image
+router.post(
+  "/imageLibrary/create",
+  upload.single("image"),
+  imageLibraryCtrl.create
+);
+
+// update image
+router.post("/imageLibrary/update/:id", imageLibraryCtrl.update);
+
+// retrieve images
+router.post("/imageLibrary", imageLibraryCtrl.retrieve);
 
 module.exports = router;
